b7f91ce8a1b9510b40604a956b56ed8a476a8542
I-The-Bill <blowery96@icloud.com>
2023-03-08 17:43:28 -0500

    Disabled CSRF

--- App/app.py
+++ App/app.py
@@ -10,40 +10,40 @@ import city
 
 
 app = Flask(__name__)
-csrf = CSRFProtect()
+'''csrf = CSRFProtect()
 csrf.init_app(app)
 
 
 import os
 SECRET_KEY = os.urandom(32)
 print(SECRET_KEY)
-app.config['SECRET_KEY'] = "bob"
+app.config['SECRET_KEY'] = "bob"'''
 
 #####COUNTRIES#####
 
 #CREATE API (POST)
 @app.post("/countries")
-@csrf.exempt
+#@csrf.exempt
 def create_country_app():
     data = request.json
     return country_c.create_country_coun(data)
 
 #READ API (GET)
 @app.get("/countries")
-@csrf.exempt
+#@csrf.exempt
 def get_all_countries_app():
     return country_c.get_all_countries_coun()
 
 #UPDATE (PUT)
 @app.put("/countries/<int:country_id>")
-@csrf.exempt
+#@csrf.exempt
 def update_country_app(country_id):
     data = request.json
     return country_c.update_country_coun(country_id,data)
 
 #DELETE 
 @app.delete("/countries/<int:country_id>")
-@csrf.exempt
+#@csrf.exempt
 def delete_country_app(country_id):
     return country_c.delete_country_coun(country_id)
 
@@ -59,7 +59,7 @@ def get_all_countries_from_continent_app(continent):
 
 #CREATE API (POST)
 @app.post("/cities")
-@csrf.exempt
+#@csrf.exempt
 def create_city_app_app():
     data = request.json
     return city.create_city_city(data)
@@ -71,14 +71,14 @@ def get_all_cities_app():
 
 #UPDATE (PUT)
 @app.put("/cities/<int:city_id>")
-@csrf.exempt
+#@csrf.exempt
 def update_city_app(city_id):
     data = request.json
     return city.update_city_city(city_id,data)
 
 #DELETE 
 @app.delete("/cities/<int:city_id>")
-@csrf.exempt
+#@csrf.exempt
 def delete_city_app(city_id):
     return city.delete_city_city(city_id)
 
